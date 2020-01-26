b5776408a1bacbbaa132a143b18ceaebc232374a
vinothbabukm <vinoth.babu@cvshealth.com>
2020-01-26 18:29:14 -0500

    changed the build path

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/v-table'));
+app.use(express.static(__dirname + '/angular-build'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/v-table/index.html'));
+  res.sendFile(path.join(__dirname + 'angular-build/index.html'));
 });
 
 // default Heroku PORT
