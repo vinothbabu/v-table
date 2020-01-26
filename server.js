9636c34b2b8b7d12e1106b8c2c40cca16aa729b4
vinothbabukm <vinoth.babu@cvshealth.com>
2020-01-26 18:10:30 -0500

    Changed dist file path

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/account-poc'));
+app.use(express.static(__dirname + '/dist/v-table'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/account-poc/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/v-table/index.html'));
 });
 
 // default Heroku PORT
