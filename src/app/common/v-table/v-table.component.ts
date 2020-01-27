c5cb50585738faab86a9acccc766b47717b1c2bc
vinothbabukm <vinoth.babu@cvshealth.com>
2020-01-26 21:14:12 -0500

    Heroku changes

--- src/app/common/v-table/v-table.component.ts
+++ src/app/common/v-table/v-table.component.ts
@@ -24,21 +24,21 @@ export class VTableComponent implements OnInit {
   isSorting(name: string) {
     return this.options.config.sortBy !== name && name !== '';
   };
-  
+
   isSortAsc(name: string) {
-    var isSortAsc: boolean = this.options.config.sortBy === name && 
+    var isSortAsc: boolean = this.options.config.sortBy === name &&
       this.options.config.sortDirection === 'asc';
     return isSortAsc;
   };
-  
+
   isSortDesc(name: string) {
-    var isSortDesc: boolean = this.options.config.sortBy === name && 
+    var isSortDesc: boolean = this.options.config.sortBy === name &&
       this.options.config.sortDirection === 'desc';
     return isSortDesc;
   };
 
-  getValue(row, column){
-    return column.value.split('.').reduce((prev:any, curr:string) => prev[curr], row);
+  getValue(row, column) {
+    return column.value.split('.').reduce((prev: any, curr: string) => prev[curr], row);
   }
 
   sortHeader(headerName: string) {
@@ -47,28 +47,33 @@ export class VTableComponent implements OnInit {
         this.options.config.sortDirection = this.options.config.sortDirection === 'asc' ? 'desc' : 'asc'
       }
       this.options.config.sortBy = headerName;
- 
+
       this.sort(this.sortedData, this.options.config.sortBy, this.options.config.sortDirection);
     }
   }
 
-  private sort = (arr,field, reverse) => {
-    return arr.sort((a,b) => {
+  private sort = (arr, field, reverse) => {
+    return arr.sort((a, b) => {
+      let x = reverse === "asc" ? a : b
+      let y = reverse === "asc" ? b : a
 
-        let x = reverse === "asc" ? a : b
-        let y = reverse === "asc" ? b : a
-    
-        if(isNaN(arr[0][field])){
-          if(isNaN(arr[0][field].split("-")[1])){
-            return x[field].localeCompare(y[field])
-          }
-        }else{
-            return x[field] - y[field] 
+      let propType = arr[0][field]
+      if (isNaN(propType)) {
+        if (propType.indexOf("-") > -1 && !isNaN(propType.split("-")[1].trim())) {
+          let xTest = x[field].split("-")[1].trim()
+          let yTest = y[field].split("-")[1].trim()
+
+          return xTest - yTest
         }
+
+        return x[field].localeCompare(y[field])
+      } else {
+        return x[field] - y[field]
+      }
     })
   }
 
-  ngOnDestroy(){
+  ngOnDestroy() {
     this._sortDataSub.unsubscribe();
   }
 
